3a580d037f5be77ceba6c12485caf21d474e88cd
Bahattin Cinic <bahattin@brickly.io>
2017-05-29 02:10:05 +0300

    add login and user profile views

---
+++ src/containers/account/Login.js
@@ -0,0 +1,100 @@
+'use strict';
+
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import {
+  StyleProvider, Form, Item, Input, Label, Button, Text, View
+} from 'native-base';
+import getTheme from '../../theme/components';
+import Toolbar from '../../components/toolbar';
+import * as actionCreators from '../../action_creators';
+import Main from '../static/Main';
+import ForgotPassword from './ForgotPassword';
+import type { State } from '../../types';
+
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: null,
+      passwd: null,
+    };
+  }
+
+  handleLogin() {
+    requestAnimationFrame(() => {
+      this.props.login(
+        this.state.username,
+        this.state.passwd,
+        {
+          navigator: this.props.navigator,
+          component: Main,
+          reset: true
+        }
+      );
+    });
+  }
+
+  handleForgotPassword() {
+    requestAnimationFrame(() => {
+      this.props.navigate(
+        {
+          navigator: this.props.navigator,
+          component: ForgotPassword
+        }
+      );
+    });
+  }
+
+  render() {
+    return (
+      <StyleProvider style={getTheme()}>
+        <Toolbar
+          title="Login"
+          showSideBar={false}
+          navigator={this.props.navigator}
+          openSidebar={this.props.openSidebar}
+        >
+          <Content>
+            <Form>
+              <Item floatingLabel marxFormElement >
+                <Label>Username</Label>
+                <Input
+                  onChangeText={(username) => this.setState({username})}
+                  value={this.state.username}
+                />
+              </Item>
+              <Item floatingLabel marxFormElement >
+                <Label>Password</Label>
+                <Input
+                  secureTextEntry={true}
+                  onChangeText={(passwd) => this.setState({passwd})}
+                  value={this.state.passwd}
+                />
+              </Item>
+
+              <View style={{ marginBottom: 30 }} />
+
+              <View style={{ marginLeft: 10, marginRight: 10 }}>
+                <Button common block marxFormElement
+                    onPress={this.handleLogin.bind(this)}>
+                  <Text>Login</Text>
+                </Button>
+              </View>
+            </Form>
+          </Content>
+        </Toolbar>
+      </StyleProvider>
+    );
+  }
+}
+
+
+const _Wrapped = connect(
+  (state: State) => ({ loading: state.get('loading')
+                     }),
+  actionCreators
+)(Login);
+
+export default _Wrapped;
---
+++ src/containers/account/User.js
@@ -0,0 +1,68 @@
+'use strict';
+
+import React, { Component } from 'react';
+import {
+  StyleProvider,
+  Content,
+  Text,
+  Button,
+  ListItem,
+  Left,
+  Body,
+  View
+} from 'native-base';
+import { connect } from 'react-redux';
+import getTheme from '../../theme/components';
+import Toolbar from '../../components/toolbar';
+import * as actionCreators from '../../action_creators';
+import Welcome from '../static/Welcome';
+
+
+class User extends Component {
+
+  handleLogout() {
+    this.props.logout({
+      navigator: this.props.navigator,
+      component: Welcome,
+      reset: true
+    });
+  }
+
+  render() {
+    return (
+      <StyleProvider  style={getTheme()}>
+        <Toolbar
+          title={'User Profile'}
+          navigator={this.props.navigator}
+          openSidebar={this.props.openSidebar}
+        >
+          <Content style={{ padding: 10 }}>
+            <ListItem>
+              <Left>
+                  <Text>Username</Text>
+              </Left>
+              <Body>
+                <Text>{this.props.user.username}</Text>
+              </Body>
+            </ListItem>
+
+            <View style={{ marginBottom: 10 }} />
+
+            <Button dark block marxFormElement
+                onPress={this.handleLogout.bind(this)}>
+              <Text>Logout</Text>
+            </Button>
+          </Content>
+        </Toolbar>
+      </StyleProvider>
+    );
+  }
+}
+
+
+const _Wrapped = connect(
+  (state) => ({ user: state.get('user') }),
+  actionCreators
+)(User);
+
+export default _Wrapped;
